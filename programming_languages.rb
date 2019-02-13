b6a27a1b0a23d9472f97e8453b3742400ab3c486
Moses Olawoyin <m.bastford@gmail.com>
2019-02-13 00:27:49 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -2,8 +2,10 @@ require "pry"
 def reformat_languages(languages)
   new_hash = {}
   languages.each do |oo_functional, language|
-    language_details.each do|name, language_details|
-    binding.pry
+    language.each do|name, language_details|
+      language_details do |keys, values|
+      
+    end
   end
     
   end
