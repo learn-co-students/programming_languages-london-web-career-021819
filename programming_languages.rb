835fa1f6d431be87bfc3036466adb246fa6b5c64
Ali Madni <ali555m@hotmail.com>
2019-02-13 12:28:50 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -3,30 +3,13 @@ def reformat_languages(languages)
   new_hash = {}
   languages.each do |lang_style, language|
     language.each do |lang, type|
-      new_hash[lang] = type
-      if new_hash[lang].has_key?(:style)
-        new_hash[lang][:style] << lang_style
-      else
+      if !(new_hash[lang])
+        new_hash[lang] = type
         new_hash[lang][:style] = []
-        new_hash[lang][:style]  << lang_style
+
       end
+      new_hash[lang][:style] << lang_style
     end
   end
-  new_hash[:javascript][:style] << :oo
   new_hash
 end
-
-
-
-#def reformat_languages(languages)
-#  new_hash = {}
-#  languages.each do |lang_style, language|
-#    language.each do |lang, type|
-#      new_hash[lang] = type
-#      new_hash[lang][:style] = []
-#      new_hash[lang][:style]  << lang_style
-#    end
-#  end
-#  new_hash[:javascript][:style] << :oo
-#  new_hash
-#end
