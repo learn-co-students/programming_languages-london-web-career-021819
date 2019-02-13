8c6680073e80ae56f9828d5b48db8fcc69cce17b
Moses Olawoyin <m.bastford@gmail.com>
2019-02-13 02:14:13 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -3,11 +3,20 @@ def reformat_languages(languages)
   new_hash = {}
   languages.each do |oo_functional, language|
     language.each do|name, language_details|
-      language_details do |keys, values|
+      language_details.each do|type, values|
+       #binding.pry
+       if new_hash[name].nil?
+         
+         new_hash[name] = {}
       
+       end
+       new_hash[name][:style]||=[]
+       new_hash[name][:style] << oo_functional
+       new_hash[name][:type]||=values
+      end
     end
-  end
-    
-  end
-  
+ end
+ #binding.pry
+      new_hash
 end
+#:type => values, :style =>[oo_functional]
