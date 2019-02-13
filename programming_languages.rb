06ed93eaca7656d18550de1bc896334f50974dc9
Laura Royle <lauraamie.royle@gmail.com>
2019-02-13 19:20:06 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,19 +1,25 @@
-def reformat_languages(languages)
-  language_attributes = {}
+require "pry"
 
-  languages.each do |oo_or_functional, language_hash| #:oo, {:ruby => {...}}
-    language_hash.each do |language, attribute_hash| #:ruby, {:type => "interpreted"}
-      attribute_hash.each do |attribute, str_value| #:type, "interpreted"
-        if language_attributes[language].nil? #if language :ruby doesn't exist in new hash
-          language_attributes[language] = {}  #initiate a new hash for the value of :ruby
-        end # this would cause us problems if we had more than one attribute we're iterating over
-        language_attributes[language][:style] ||= []  # create a :style key within that hash and set it equal to an empty array
-        language_attributes[language][:style] << oo_or_functional  #added oo key from first level of languages hash to this key
-        if language_attributes[language][attribute].nil? #if :ruby language key doesn't have a :type key nested within it
-          language_attributes[language][attribute] = str_value  #set this :type key to equal the str "interpreted"
-        end
-      end
+def reformat_languages(languages)
+  new_hash = {}
+
+   languages.each do |oo_or_functional, language_hash| #:oo, ruby nested hash
+     language_hash.each do |language, attribute| #:ruby, type hash
+       attribute.each do |type, string| #:type, "interpreted"
+         if
+           new_hash[language].nil?
+           new_hash[language] = {} #creates a new hash if first iteration is empty
+         end
+         #then we can start to build the new has from the bottom up
+          new_hash[language][:style] ||= [] #creates empty array for style value
+          new_hash[language][:style] << oo_or_functional #shovels in element from first iteration
+         if
+           new_hash[language][type].nil?
+           new_hash[language][type] = {} #created empty array for type
+         end
+           new_hash[language][type] = string #adds string value
+       end
     end
   end
-  language_attributes
+  new_hash
 end
